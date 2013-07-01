e0fbb6c91c982ab4097a66bf8a9229a6e1c07976
Ubuntu <ubuntu@ip-10-248-35-148.us-west-2.compute.internal>
2013-07-01 21:24:33 +0000

    added some south stuff monday afternoon

--- mysite/data/views.py
+++ mysite/data/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.http import Http404
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 import simplejson as json
 from django.db.models import Q
@@ -15,22 +16,102 @@ from data.models import FeedObject
 from data.models import Answer
 from data.models import ListField
 from data.models import TrendingObject
+from data.models import ProductType
+from django.forms.models import model_to_dict
 from random import randint
 from random import choice
 import logging
 import urllib
 import random
+import datetime
+import calendar
+import requests
+
+@csrf_exempt
+def checkimage(request):
+	products = Product.objects.all()
+	array = []
+	for current in products:
+		name = current.fileURL
+		url = current.fileURL
+		url = url.replace("+", "%2b")
+		url = url.replace(" ", "+")
+		url = "https://s3.amazonaws.com/pave_product_images/" + url
+		array.append({"name":name, "url":url})
+	return render_to_response('data/checkimage.html', {'data': array})
+
+@csrf_exempt
+def rebase(request):
+    for current in ProductType.objects.all():
+        current.count = 0
+        current.save()
+
+    for current in Product.objects.all():
+        current.save()
+
+    return HttpResponse()
 
 @csrf_exempt
 def index(request):
 	return HttpResponse("Hey, you're at the index for views in data")
 
+@csrf_exempt
+def numberOfNewObjects(request, user_id, time_since):
+	count = FeedObject.objects.filter(forUser = user_id, updatedAt__gte=(time_since)).count()
+
+#		latest = FeedObject.objects.filter(forUser = user_id).latest('updatedAt').updatedAt.utctimetuple()
+#	if count != 0:
+	try:
+		most_recent = calendar.timegm(datetime.datetime.utcnow().utctimetuple())
+#		most_recent = calendar.timegm(FeedObject.objects.filter(forUser = user_id).latest('updatedAt').updatedAt.utctimetuple())
+	except:
+		most_recent = 0
+#	else:
+#		most_recent = calendar.timegm(datetime.datetime.utcnow().utctimetuple())	
+	return HttpResponse(json.dumps({'count': count, 'last':most_recent}), mimetype='application/json')
 
 @csrf_exempt
-def getTrendingObjects(request, number):
-	all_objects = TrendingObject.objects.all()[number:(int(number)+50)]
+def numberOfAnswersGiven(request, user_id):
+        count = Answer.objects.filter(fromUser = user_id).count()
 
-	response = HttpResponse(serializers.serialize("json", list(all_objects)), mimetype='application/json')
+#               latest = FeedObject.objects.filter(forUser = user_id).latest('updatedAt').updatedAt.utctimetuple()
+#       if count != 0:
+#        try:
+#                most_recent = calendar.timegm(datetime.datetime.utcnow().utctimetuple())
+#               most_recent = calendar.timegm(FeedObject.objects.filter(forUser = user_id).latest('updatedAt').updatedAt.$
+#        except:
+#                most_recent = 0
+#       else:
+#               most_recent = calendar.timegm(datetime.datetime.utcnow().utctimetuple())        
+        return HttpResponse(json.dumps([count]), mimetype='application/json')
+
+@csrf_exempt
+def numberOfAnswersGivenForJS(request, user_id):
+	count = Answer.objects.filter(fromUser = user_id).count()
+	response = HttpResponse(json.dumps([count]), mimetype='application/json')
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+        response["Access-Control-Max-Age"] = "1000"
+        response["Access-Control-Allow-Headers"] = "*"
+	return response
+
+
+@csrf_exempt
+def getTrendingObjects(request, user_id):
+	unique_types = TrendingObject.objects.values_list('type', flat=True).distinct()
+	generated_objects = []
+	for cur_type in unique_types:
+		current_list = {}
+		current_list[cur_type] = []
+		cur_objects = TrendingObject.objects.filter(type = cur_type)
+		for cur_object in cur_objects:
+			cur_object.forUser = user_id
+			cur_object.question_text = cur_object.question_text.replace("%n", "you")
+			current_list[cur_type].append(model_to_dict(cur_object))
+
+		generated_objects.append(current_list)
+
+	response = HttpResponse(json.dumps((generated_objects)), mimetype='application/json')
         response["Access-Control-Allow-Origin"] = "*"
         response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
         response["Access-Control-Max-Age"] = "1000"
@@ -61,7 +142,7 @@ def getListQuestions(request, user_id):
 
         #get all feed items by friends
         current_user = User.objects.get(pk=user_id)
-        friends_objects = FeedObject.objects.filter(pk__in=current_user.friendsInApp)
+        friends_objects = FeedObject.objects.filter(forUser__in=current_user.friendsInApp)
         #friends_objects = friends_objects.filter(product1Count__in=[1,2,3]) 
         #friends_objects = friends_objects.filter(product2Count__in=[1,2,3])[:50]
         friends_objects = friends_objects[:50]
@@ -105,8 +186,12 @@ def getListQuestions(request, user_id):
                         current_object.product2Count = 0
                         current_object.currentQuestion = currentQuestion
                         current_object.questionText = currentQuestion.text
+#			current_object.questionText = "New here"
                 else:
                         current_object = old_objects[0]
+
+#		current_object.questionText = current_user.names[current_user.friends.index(current_friend)]
+#		current_object.questionText = "New q"
 
                 list_friends_objects.append(current_object)
 
@@ -134,41 +219,62 @@ def getListQuestionsNew(request, user_id):
 	
 	#get all feed items by friends
 	current_user = User.objects.get(pk=user_id)
-	friends_objects = FeedObject.objects.filter(forUser__in=current_user.friendsInApp)
+#	friends_objects = FeedObject.objects.filter(forUser__in=current_user.friendsInApp)
+#	return HttpResponse(friends_objects.count())
 	#friends_objects = friends_objects.filter(product1Count__in=[1,2,3]) 
 	#friends_objects = friends_objects.filter(product2Count__in=[1,2,3])[:50]
-	friends_objects = friends_objects[:50]
-	num_friends_objects = friends_objects.count()
-	all_friends_objects = list(friends_objects)
+#	friends_objects = friends_objects[:50]
+#	num_friends_objects = friends_objects.count()
+#	all_friends_objects = list(friends_objects)
 	list_friends_objects = []
 
-	for old_object in all_friends_objects:
-		current_object = {}
-		current_object['product1'] = old_object.product1.id
-                current_object['product2'] = old_object.product2.id
-                current_object['image1'] = old_object.image1
-                current_object['image2'] = old_object.image2
-                current_object['fbFriend1'] = old_object.fbFriend1
-                current_object['fbFriend2'] = old_object.fbFriend2
-                current_object['product1Count'] = old_object.product1Count
-                current_object['product2Count'] = old_object.product2Count
-                current_object['currentQuestion'] = old_object.currentQuestion.id
-                current_object['questionText'] = old_object.questionText
+#	for old_object in all_friends_objects:
+#		current_object = {}
+#		current_object['product1'] = old_object.product1.id
+#               current_object['product2'] = old_object.product2.id
+#                current_object['image1'] = old_object.image1
+#                current_object['image2'] = old_object.image2
+#                current_object['fbFriend1'] = old_object.fbFriend1
+#                current_object['fbFriend2'] = old_object.fbFriend2
+#                current_object['product1Count'] = old_object.product1Count
+#                current_object['product2Count'] = old_object.product2Count
+#                current_object['currentQuestion'] = old_object.currentQuestion.id
+#                current_object['questionText'] = old_object.questionText
 		
-		index = random.randint(0,len(current_user.friends) - 1)
-                friend = str(current_user.friends[index])
-                name = current_user.names[index]
+#		index = random.randint(0,len(current_user.friends) - 1)
+#                friend = str(current_user.friends[index])
+#                name = current_user.names[index]
 
-                current_object['name'] = name
-                current_object['friend'] = friend
+#                current_object['name'] = name
+#                current_object['friend'] = friend
 
-		list_friends_objects.append(current_object)
+#		list_friends_objects.append(current_object)
 
+#	return HttpResponse(json.dumps(list_friends_objects))
 
 	#now creates the number of questions by hand
-	num_new_objects = 100 - num_friends_objects
+#	num_new_objects = 100 - num_friends_objects
+	num_new_objects = 100
 	for x in range(num_new_objects):
-		current_friend = choice(current_user.friends)
+#		current_friend = choice(current_user.friends)
+
+		name = ""
+		while name == "":
+			if len(current_user.friends) < 100:	
+				index = random.randint(0,len(current_user.friends) - 1)
+			else:
+				decision = random.random()
+				if decision < 0.8:
+					index = random.randint(0, 100-1)
+				else:
+					index = random.randint(100,len(current_user.friends) - 1)
+
+	                current_friend = str(current_user.friends[index])
+        	        name = current_user.names[index]
+			#checks if it is a valid name (in a very hackish way)
+			if name != name.decode('utf8'):
+				name = ""
+			
 
 		#gets a random quesiton
         	num_questions = Question.objects.count() -1
@@ -180,7 +286,7 @@ def getListQuestionsNew(request, user_id):
         	#now gets two random products in that type
         	num_products = current_type.count - 1
         	index1 = randint(1, num_products)  
-        	index2 = randint(1, num_products - 1) 
+        	index2 = randint(1, num_products) -1 
        		if index1 == index2: index2 = num_products
 
        		currentProduct1 = Product.objects.get(type = current_type, idInType = index1)
@@ -215,12 +321,13 @@ def getListQuestionsNew(request, user_id):
                         current_object['currentQuestion'] = old_objects[0].currentQuestion.id
                         current_object['questionText'] = old_objects[0].questionText
 
-		index = random.randint(0,len(current_user.friends) - 1)
-                friend = str(current_user.friends[index])
-                name = current_user.names[index]
+		try:
+			current_object['questionText'] = current_object['questionText'].replace("%n", name.split()[0])
+		except:
+			current_object['questionText'] = current_object['questionText']
 
 		current_object['name'] = name
-		current_object['friend'] = friend
+		current_object['friend'] = current_friend
 		
 		list_friends_objects.append(current_object)
 
@@ -383,6 +490,87 @@ def newAnswer(request):
                 return response
         return HttpResponse()
 
+@csrf_exempt
+def batchCreateProducts(request):
+	# to save a product we need product type
+	
+	debug =  HttpResponse(str(json.loads(request.POST["data"])), mimetype="application/json")
+	debug["Access-Control-Allow-Origin"] = "*"
+	debug["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+	debug["Access-Control-Max-Age"] = '1000'
+	debug["Access-Control-Allow-Headers"] = "*"
+	if request.method == "POST":
+		allProducts = json.loads(request.POST['data'])
+		for currentRow in allProducts:
+			product_type = ProductType.objects.get(text = currentRow['type'])
+			if product_type:
+				obj = Answer.objects.create(
+					type = product_type,
+					description = currentRow["description"],
+					fileURL = currentRow['filename'],
+					idInType = 0
+				)
+		response = HttpResponse(len(json.loads(request.POST['data'])), mimetype = 'application/json')
+		response["Access-Control-Allow-Origin"] = "*"
+		response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+		response["Access-Control-Max-Age"] = "1000"
+		response["Access-Control-Allow-Headers"] = "*"
+		return response
+	return HttpResponse("Error, not a POST Request")
+
+@csrf_exempt
+def batchCreateAnswers(request):
+	if request.method == 'POST':
+#	        response = HttpResponse("Up in here, up in here", mimetype = 'application/json')
+		allAnswers = json.loads(request.POST['data'])
+
+		for currentRow in allAnswers:
+			obj = Answer.objects.create(
+                 		fromUser = User.objects.get(pk=currentRow['userID']), 
+                       		forFacebookId = currentRow['friend'],
+  	                	chosenProduct =  Product.objects.get(pk=currentRow['chosen']),
+                        	wrongProduct =  Product.objects.get(pk=currentRow['wrong']),
+                        	question = Question.objects.get(pk=currentRow['question'])
+                	)
+		response = HttpResponse(len(json.loads(request.POST['data'])), mimetype = 'application/json')
+                response["Access-Control-Allow-Origin"] = "*"
+                response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+                response["Access-Control-Max-Age"] = "1000"
+                response["Access-Control-Allow-Headers"] = "*"		
+		return response
+	return HttpResponse("Didn't work... Probably because this wasn't a POST request")
+
+@csrf_exempt
+def transferAnswers(request):
+	now = datetime.datetime.now()
+	earlier = now - datetime.timedelta(hours = 1)
+	data = []
+	recent_answers = Answer.objects.filter(created_at__range=(earlier,now))
+	for answer in recent_answers:
+		data.append({"forFacebookId": answer.forFacebookId, "fromUser": answer.fromUser.pk, "chosenProduct": answer.chosenProduct.id, "wrongProduct": answer.wrongProduct.id})
+	# send a POST to ec2- with the answers
+	url = "http://ec2-54-218-218-2.us-west-2.compute.amazonaws.com/data/uploadanswers"
+	r = requests.post(url, data={"data": json.dumps(data)})
+	
+	response = HttpResponse(r.text)
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+        response["Access-Control-Max-Age"] = "1000"
+        response["Access-Control-Allow-Headers"] = "*"
+	return response
+
+@csrf_exempt
+def getListQuestionsForPersonalityType(request, personality_type):
+	types_to_rec = ["sports", "lottery", "careers"]
+	for p_type in types_to_rec:
+		product_type = ProductType.objects.get(text=p_type)
+		products = Products.objects.filter(type=product_type)
+		p_combs = itertools.combinations(products, 2)
+		a = []
+		for p_tuple in p_combs:
+			a.append(p_tuple)	
+			# append to an array and send back					
+	return HttpResponse("sucks to suck")
 
 @csrf_exempt
 def newUser(request):
