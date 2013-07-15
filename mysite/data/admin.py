172ef2bc59014dede3c2118fe03e6b3b5e152073
Your Name <you@example.com>
2013-07-15 05:08:03 +0000

    added new models to admin

--- mysite/data/admin.py
+++ mysite/data/admin.py
@@ -12,16 +12,29 @@ from data.models import UserScore
 from data.models import TrendingObject
 from data.models import UserGeneratedQuestion, UserGeneratedProduct, UserGeneratedAnswer, QuestionQueue 
 from data.models import QuestionObject 
+from data.models import TrainingQuestion, TrainingAnswer, TrainingProduct, TrainingProductType
 
+admin.site.register(User)
+
+# TRAINING
+admin.site.register(TrainingQuestion)
+admin.site.register(TrainingAnswer)
+admin.site.register(TrainingProduct)
+admin.site.register(TrainingProductType)
+
+# GAME PLAY
 admin.site.register(QuestionObject)
 admin.site.register(Question)
 admin.site.register(ProductType)
 admin.site.register(Product)
-admin.site.register(User)
 admin.site.register(FeedObject)
 admin.site.register(Answer)
-admin.site.register(TrendingObject)
+
+# USER GENERATED
 admin.site.register(UserGeneratedQuestion)
 admin.site.register(UserGeneratedProduct)
 admin.site.register(UserGeneratedAnswer)
 admin.site.register(QuestionQueue)
+
+# TRENDING
+admin.site.register(TrendingObject)
