c3727c71ad149880864f0e8eccaae28a8b001a12
Your Name <you@example.com>
2013-07-19 01:19:59 +0000

    Make top friends blank, before push to production

--- mysite/data/models.py
+++ mysite/data/models.py
@@ -43,7 +43,7 @@ class User(models.Model):
         facebookID = models.CharField(max_length=200, primary_key=True)
         socialIdentity = models.TextField(blank=True)
         profile = models.TextField(blank=True)
-	topFriends = ListField()
+	topFriends = ListField(blank=True)
         friends = ListField()
         names = ListField()
         genders = ListField()
